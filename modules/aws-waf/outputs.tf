fb3586a2348dc16f3d74685466d7e91834ecd5d4
Mateusz Kozakiewicz <mateusz.kozakiewicz@boldare.com>
2020-03-23 01:42:38 +0100

    fix(waf): if no waf_acl created output null

--- modules/aws-waf/outputs.tf
+++ modules/aws-waf/outputs.tf
@@ -1,4 +1,4 @@
 output "web_acl_id" {
-  value       = aws_waf_web_acl.waf_acl[0].id
+  value       = length(aws_waf_web_acl.waf_acl) > 0 ? aws_waf_web_acl.waf_acl[0].id : null
   description = "Web Application Firewall service ID limiting access to CloudFront distribution to given IP adresses."
 }
