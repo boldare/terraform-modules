06b0eb55d2c21ebd9192acbaf27ff7ea05f074d3
Michal Trochimiuk <michal.trochimiuk@boldare.com>
2020-04-08 11:15:58 +0200

    fixed example in order to use Ec2 instead ASG

--- modules/aws-bastion-instance/example/main.tf
+++ modules/aws-bastion-instance/example/main.tf
@@ -45,16 +45,15 @@ ${module.admin_ssh_keys.user_data_chunk}
 
 EOF
   enable_monitoring    = true
-  vpc_id               = var.vpc_id
+  public_subnet_id       = var.public_subnet_id
+  vpc_id = var.vpc_id
+  disable_api_termination = true
 
   # Access
   ssh_key_name           = "example-key"
   allowed_cidr_blocks    = [
     "18.202.145.21/32", # Boldare VPN IP
   ]
-  vpc_public_subnet_tags = {
-    "MyVpcSubnetType" = "Public"
-  }
 }
 
 resource "aws_iam_role_policy_attachment" "bastion_ssh_policy" {
--- modules/aws-bastion-instance/example/variables.tf
+++ modules/aws-bastion-instance/example/variables.tf
@@ -1,3 +1,7 @@
 variable "vpc_id" {
   type = string
 }
+
+variable "public_subnet_id" {
+  type = string
+}
