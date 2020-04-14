612c76edf0c99f613d4bbd4418923db623c52e26
Grzegorz Mucha <grzegorz.mucha@boldare.com>
2020-04-14 12:52:03 +0200

    ECS-service-permission example

---
+++ modules/aws-ecs-service-permissions/example/main.tf
@@ -0,0 +1,42 @@
+module "app_role" {
+  source = "../"
+
+  name = "boldare-dev"
+  attached_policies = [
+  aws_iam_policy.example.arn]
+  secret_arns = [
+  aws_secretsmanager_secret.app_secret.arn]
+
+}
+
+# Secret
+
+resource "random_password" "app_secret" {
+  length = 50
+}
+
+resource "aws_secretsmanager_secret" "app_secret" {
+  name = "boldare/app"
+}
+
+resource "aws_secretsmanager_secret_version" "app_secret_v1" {
+  secret_id     = aws_secretsmanager_secret.app_secret.id
+  secret_string = random_password.app_secret.result
+}
+
+# policy
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    actions = [
+    "*"]
+    resources = [
+    "*"]
+  }
+}
+
+resource "aws_iam_policy" "example" {
+  name   = "example_policy"
+  policy = data.aws_iam_policy_document.example.json
+}
+
