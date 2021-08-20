f35b5898a3ab12f2ed5bb5fd0955dd84e83e3c22
Grzegorz <13075554+grzesjam@users.noreply.github.com>
2021-08-20 15:20:08 +0200

    Naming clarification

--- modules/aws-acm-multi-zone/output.tf
+++ modules/aws-acm-multi-zone/output.tf
@@ -1,9 +1,9 @@
 output "list_of_domains" {
-  value = local.domains
+  value       = local.domains
   description = "Domains that ACM certificate was created for"
 }
 
 output "certificate_arn" {
-  value = aws_acm_certificate.cert.arn
+  value       = aws_acm_certificate.cert.arn
   description = "ARN of ACM certificate"
 }
