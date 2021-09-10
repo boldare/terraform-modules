bfc7df7a370a05846fe8d87d2ddc45ba9775b3d5
Grzegorz <13075554+grzesjam@users.noreply.github.com>
2021-09-10 12:38:06 +0200

    Better example with usage of policy

--- modules/oracle-container-registry/example/main.tf
+++ modules/oracle-container-registry/example/main.tf
@@ -21,3 +21,34 @@ module "container_registry_api_php" {
   container_repository_is_immutable = false
   container_repository_is_public    = false
 }
+
+module "container_registry_spa" {
+  source = "./.."
+
+  compartment_id = local.tenancy
+  project_name   = local.project_name
+  environment    = local.environment
+  region         = "me-jeddah-1"
+  suffix         = "spa"
+
+  group_name_for_permission         = "${local.project_name}-${local.environment}-CICD"
+  container_repository_is_immutable = false
+  container_repository_is_public    = false
+}
+
+module "user" {
+  source = "../../oracle-user"
+
+  tenancy_ocid        = local.tenancy
+  compartment_id      = local.tenancy
+  environment         = local.environment
+  user_name           = "${local.project_name}-${local.environment}-CICD"
+  group_name          = "${local.project_name}-${local.environment}-CICD"
+  create_auth_tokens  = true
+
+  policy_name = "${local.project_name}-${local.environment}-access-to-cr"
+  policy_statements = flatten([
+    module.container_registry_api_php.policy,
+    module.container_registry_spa.policy
+  ])
+}
